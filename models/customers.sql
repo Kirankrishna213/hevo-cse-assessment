8a3371f0a1aaf53f15651a19c08de8d9c8c8a960
Kirankrishna213 <kkirankrishnar@gmail.com>
2026-04-28 17:22:35 +0530

    Update customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -1,42 +1,20 @@
-WITH customers AS (
-    SELECT * FROM {{ ref('raw_customers') }}
-),
+{{ config(materialized='table') }}
 
-orders AS (
-    SELECT * FROM {{ ref('raw_orders') }}
-),
-
-payments AS (
-    SELECT * FROM {{ ref('raw_payments') }}
-),
-
-customer_orders AS (
-    SELECT
-        user_id AS customer_id,
-        MIN(order_date) AS first_order,
-        MAX(order_date) AS most_recent_order,
-        COUNT(*) AS number_of_orders
-    FROM orders
-    GROUP BY user_id
-),
-
-customer_payments AS (
-    SELECT
-        o.user_id AS customer_id,
-        SUM(p.amount) AS customer_lifetime_value
-    FROM payments p
-    JOIN orders o ON p.order_id = o.id
-    GROUP BY o.user_id
-)
-
-SELECT
+SELECT 
     c.id AS customer_id,
     c.first_name,
     c.last_name,
-    co.first_order,
-    co.most_recent_order,
-    co.number_of_orders,
-    cp.customer_lifetime_value
-FROM customers c
-LEFT JOIN customer_orders co ON c.id = co.customer_id
-LEFT JOIN customer_payments cp ON c.id = cp.customer_id
+
+    MIN(o.order_date) AS first_order,
+    MAX(o.order_date) AS most_recent_order,
+    COUNT(DISTINCT o.id) AS number_of_orders,
+
+    SUM(p.amount) AS customer_lifetime_value
+
+FROM {{ ref('raw_customers') }} c
+LEFT JOIN {{ ref('raw_orders') }} o 
+    ON c.id = o.customer_id
+LEFT JOIN {{ ref('raw_payments') }} p 
+    ON o.id = p.order_id
+
+GROUP BY c.id, c.first_name, c.last_name
